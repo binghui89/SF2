5fdf16e7213c82d8cb1c51ddef59779c36f864c7
Binghui Li <bli6@ncsu.edu>
2019-12-03 10:39:16 -0600

    Remove the part that remove empty lines.
    This causes problem in the past, I realize it better to leave rows with zero coefficients as is, just don't remove it so we have the entire interval of the convolved results there.

--- conv_poly.m
+++ conv_poly.m
@@ -1,4 +1,4 @@
-function [a_convoluted, t_convoluted] = conv_poly(a1, t1, a2, t2, delta_t)
+function [a_convolved, t_convolved] = conv_poly(a1, t1, a2, t2, delta_t)
 % Piecewise polynomial convolution
 % USE: [a_convoluted, t_convoluted] = CONV_POLY(a1, t1, a2, t2, delta_t)
 %
@@ -43,63 +43,57 @@ function [a_convoluted, t_convoluted] = conv_poly(a1, t1, a2, t2, delta_t)
 % function forms
 c1 = c_by_a_and_t(a1, t1);
 c2 = c_by_a_and_t(a2, t2);
+% Use integer offset since the unique function works better with integers
+t1_intoffset = round((t1 - t1(1))./delta_t);
+t2_intoffset = round((t2 - t2(1))./delta_t);
 
 % Convert both functions from the delta function form to the triplet form
-d1 = to_triplet_form(c1, t1);
-d2 = to_triplet_form(c2, t2);
+% Each row is [coefficient, offset, degree]
+% d1 = to_triplet_form(c1, t1);
+% d2 = to_triplet_form(c2, t2);
+d1 = to_triplet_form(c1, t1_intoffset);
+d2 = to_triplet_form(c2, t2_intoffset);
 r1 = size(d1, 1);
 r2 = size(d2, 1);
 
-d_convoluted = nan(r1*r2, 1);
+d_convolved = nan(r1*r2, 1);
 for i = 1: 3
     tmp1 = repmat(d1(:, i), r2, 1);
     tmp2 = repmat(d2(:, i), 1, r1)'; tmp2 = tmp2(:);
     if i == 1
-        d_convoluted(:, i) = tmp1.*tmp2;
+        d_convolved(:, i) = tmp1.*tmp2;
     else
-        d_convoluted(:, i) = tmp1 + tmp2;
+        d_convolved(:, i) = tmp1 + tmp2;
     end
 end
 
 % Merge similar terms (the same offset and degree)
-tc1 = t1(1) + t2(1); % The left end of the starting interval of the convoluted distribution
-int_offset = round((d_convoluted(:, 2) - tc1)/delta_t);
-int_degree = round(d_convoluted(:, 3));
-int_offset_degree = [int_offset int_degree];
+tc1 = t1(1) + t2(1); % The left end of the first interval of the convolved distribution
+% int_offset = round((d_convoluted(:, 2) - tc1)/delta_t);
+% int_degree = round(d_convoluted(:, 3));
+int_offset_degree = d_convolved(:, 2:3);
 int_offset_degree_unique = unique(int_offset_degree, 'row');
-d_convoluted_unique_intoffset = nan(size(int_offset_degree_unique, 1), 3);
-for i = 1: size(d_convoluted_unique_intoffset, 1)
+d_convolved_unique_intoffset = nan(size(int_offset_degree_unique, 1), 3);
+for i = 1: size(d_convolved_unique_intoffset, 1)
     row_selected = (...
-        int_offset == int_offset_degree_unique(i, 2-1) ...
-        & int_degree == int_offset_degree_unique(i, 3-1) ...
+        d_convolved(:, 2) == int_offset_degree_unique(i, 2-1) ...
+        & d_convolved(:, 3) == int_offset_degree_unique(i, 3-1) ...
         );
-    d_convoluted_unique_intoffset(i, 1) = sum(d_convoluted(row_selected), 1);
-    d_convoluted_unique_intoffset(i, 2:3) = int_offset_degree_unique(i, 1:2);
+    d_convolved_unique_intoffset(i, 1) = sum(d_convolved(row_selected), 1);
+    d_convolved_unique_intoffset(i, 2:3) = int_offset_degree_unique(i, 1:2);
 end
 
-
-% d_convoluted_unique = unique(int_offset_degree, 'row');
-% % d_convoluted_unique = unique(d_convoluted(:, 2:3), 'row');
-% d_convoluted_unique = [nan(size(d_convoluted_unique, 1), 1), d_convoluted_unique];
-% for i = 1: size(d_convoluted_unique, 1)
-%     row_selected = (...
-%         d_convoluted(:, 2) == d_convoluted_unique(i, 2) ...
-%         & d_convoluted(:, 3) == d_convoluted_unique(i, 3) ...
-%         );
-%     d_convoluted_unique(i, 1) = sum(d_convoluted(row_selected), 1);
-% end
-
 % Remove terms with zero coefficents
-d_convoluted_reduced_intoffset = d_convoluted_unique_intoffset;
-d_convoluted_reduced_intoffset(d_convoluted_reduced_intoffset(:, 1)==0, :)=[];
+% d_convoluted_reduced_intoffset = d_convolved_unique_intoffset;
+% d_convoluted_reduced_intoffset(d_convoluted_reduced_intoffset(:, 1)==0, :)=[];
 
 % Convert the resulting triplet form to the delta function forms
-[c_convoluted, t_convoluted_intoffset] = from_triplet_form(d_convoluted_reduced_intoffset);
+[c_convolved, t_convolved_intoffset] = from_triplet_form(d_convolved_unique_intoffset);
 
 % Conver the resulting delta function forms into the piecewise polynomial
 % function forms
-t_convoluted = t_convoluted_intoffset.*delta_t + tc1;
-a_convoluted = a_by_c_and_t(c_convoluted, t_convoluted);
+t_convolved = t_convolved_intoffset.*delta_t + tc1;
+a_convolved = a_by_c_and_t(c_convolved, t_convolved);
 end
 
 function [c, t] = from_triplet_form(d)
@@ -121,5 +115,5 @@ function d = to_triplet_form(c, t)
 t_matrix = repmat(t, 1, col);
 degree_matrix = -repmat(1:col, row, 1);
 d = [c(:) t_matrix(:) degree_matrix(:)];
-d(d(:, 1)==0, :) =[];
+% d(d(:, 1)==0, :) =[];
 end
