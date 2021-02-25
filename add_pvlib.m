29f37c0537af71824fce58560910783b8e505e67
binghui89 <bli6@ncsu.edu>
2021-02-25 15:05:33 -0600

    Add Ganymede path.

--- add_pvlib.m
+++ add_pvlib.m
@@ -1,6 +1,12 @@
 function add_pvlib()
 % Add path and Site ID mapping
-addpath('C:\Users\bxl180002\git\MATLAB_PV_LIB');
-addpath('C:\Users\bxl180002\git\MATLAB_PV_LIB\Example Data');
-addpath('C:\Users\bxl180002\git\MATLAB_PV_LIB\Required Data');
+if ispc
+    addpath('C:\Users\bxl180002\git\MATLAB_PV_LIB');
+    addpath('C:\Users\bxl180002\git\MATLAB_PV_LIB\Example Data');
+    addpath('C:\Users\bxl180002\git\MATLAB_PV_LIB\Required Data');
+elseif isunix
+    addpath('/home/bxl180002/git/MATLAB_PV_LIB');
+    addpath('/home/bxl180002/git/MATLAB_PV_LIB/Example Data');
+    addpath('/home/bxl180002/git/MATLAB_PV_LIB/Required Data');
+end
 end
