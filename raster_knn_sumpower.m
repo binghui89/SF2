3b3d4f4f425cf040093213d2866497bb3d88d839
Binghui Li <bli6@ncsu.edu>
2021-02-28 22:32:29 -0600

    Add simple irradiance-to-power conversion.

--- raster_knn_sumpower.m
+++ raster_knn_sumpower.m
@@ -19,6 +19,20 @@ T_rtpd_forcong = T_rtpd_forcong(:, {'HOUR_START', 'max_error_max', 'min_error_mi
 T_rtpd_forcong.Properties.VariableNames{'max_error_max'} = 'FRU';
 T_rtpd_forcong.Properties.VariableNames{'min_error_min'} = 'FRD';
 
+% Calculate PV power using the simple way
+t = 25 + ghi_cs_cell./800.*(45.5-20);
+p1_solar_cs = ghi_cs_cell./1000.*(1+0.004.*(t - 25));
+p1_solar = nan(size(ar_datetime, 1), size(cell_pvcellghi{1}, 2), 5);
+for i = 1:5
+    t = 25 + cell_pvcellghi{i}./800.*(45.5-20);
+    p1_solar(:, :, i) = cell_pvcellghi{i}./1000.*(1+0.004.*(t - 25));
+end
+pvlib_flag = input('PVlib? (1 - Yes/0 - No): ');
+if ~pvlib_flag
+    p_solar_cs = p1_solar_cs;
+    p_solar = p1_solar;
+end
+
 %% Run baseline and kNN using sum of PVlib power
 % Baseline run
 karray = 5:5:40; % the number of nearest neighbors
@@ -56,14 +70,20 @@ plot(...
     'r' ...
     );
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Calculate classifier based on kpv
+%% Calculate classifier based on kpv
 sigma_psolar = nan(size(p_solar, 1), 5);
 % plot(sum(squeeze(p_solar(:, T_eia860_caiso{:, 'within_boundary'}==1, 3)*diag(T_eia860_caiso{T_eia860_caiso{:, 'within_boundary'}==1, 'NameplateCapacity_MW_'})), 2));
-for q = 1:5
-    sigma_psolar(:, q) = sum(squeeze(p_solar(:, T_eia860_caiso{:, 'within_boundary'}==1, 3)*diag(T_eia860_caiso{T_eia860_caiso{:, 'within_boundary'}==1, 'NameplateCapacity_MW_'})), 2);
+if pvlib_flag
+    for q = 1:5
+        sigma_psolar(:, q) = sum(squeeze(p_solar(:, T_eia860_caiso{:, 'within_boundary'}==1, 3)*diag(T_eia860_caiso{T_eia860_caiso{:, 'within_boundary'}==1, 'NameplateCapacity_MW_'})), 2);
+    end
+    sigma_psolarcs = sum(p_solar_cs(:, T_eia860_caiso{:, 'within_boundary'}==1)*diag(T_eia860_caiso{T_eia860_caiso{:, 'within_boundary'}==1, 'NameplateCapacity_MW_'}), 2);
+else
+    for q = 1:5
+        sigma_psolar(:, q) = sum(squeeze(p_solar(:, :, 3)*diag(T_pvcell{T_pvcell{:, 'within_boundary'}==1, 'TotalCapacity'})), 2);
+    end
+    sigma_psolarcs = sum(p_solar_cs*diag(T_pvcell{T_pvcell{:, 'within_boundary'}==1, 'within_boundary'}), 2);
 end
-sigma_psolarcs = sum(p_solar_cs(:, T_eia860_caiso{:, 'within_boundary'}==1)*diag(T_eia860_caiso{T_eia860_caiso{:, 'within_boundary'}==1, 'NameplateCapacity_MW_'}), 2);
 
 kpv = sigma_psolar./repmat(sigma_psolarcs, 1, 5);
 
@@ -111,7 +131,8 @@ for c = 1:4
             this_hour = T_knnkpv_rtpd.HOUR_START.Hour(i);
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             % Can use the days AFTER this_date if results are not ideal
-            T_sample = T_classifier_kpv((T_classifier_kpv.DATE<this_date)&(T_classifier_kpv.HOUR_START.Hour==this_hour), :);
+            T_sample = T_classifier_kpv((T_classifier_kpv.HOUR_START.Hour==this_hour), :);
+%             T_sample = T_classifier_kpv((T_classifier_kpv.DATE<this_date)&(T_classifier_kpv.HOUR_START.Hour==this_hour), :);
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             if any(isnan(T_knnkpv_rtpd{i, 'c'}))
                 T_sample_sorted = T_sample(T_sample.DATE>=this_date-days(k), :); % We use 30 previous days, i.e., baseline, if data is nan
@@ -134,8 +155,7 @@ for c = 1:4
     cell_knnkpv_rtpd{c} = T_knnkpv_rtpd;
 end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Process results and visualize
+%% Process results and visualize
 f_errormax_rtpd = T_rtpd{ismember(T_rtpd.HOUR_START, T_baseline_rtpd.HOUR_START), 'error_max'}; % 15-min
 f_errormin_rtpd = T_rtpd{ismember(T_rtpd.HOUR_START, T_baseline_rtpd.HOUR_START), 'error_min'}; % 15-min
 T_errormax_rtpd = array2table(reshape(f_errormax_rtpd, 4, numel(f_errormax_rtpd)/4)', 'VariableNames', {'error_max_1', 'error_max_2', 'error_max_3', 'error_max_4'});
@@ -144,7 +164,7 @@ T_actual_error = [T_baseline_rtpd(:, 'HOUR_START') T_errormax_rtpd T_errormin_rt
 ar_actual_error_fru = T_actual_error{:, {'error_max_1', 'error_max_2', 'error_max_3', 'error_max_4'}};
 ar_actual_error_frd = T_actual_error{:, {'error_min_1', 'error_min_2', 'error_min_3', 'error_min_4'}};
 
-%% Baseline
+% Baseline
 for k = karray
     [oversupply_baseline_fru(karray==k), pshort_baseline_fru(karray==k)] = calculate_oversupply_and_pshort(ar_actual_error_fru, T_baseline_rtpd{:, strcat('FRU_k', num2str(k))});
     [oversupply_baseline_frd(karray==k), pshort_baseline_frd(karray==k)] = calculate_oversupply_and_pshort(-ar_actual_error_frd, -T_baseline_rtpd{:, strcat('FRD_k', num2str(k))});
@@ -159,11 +179,54 @@ for c = 1:4
     end
 end
 
-figure(); plot(karray, oversupply_baseline_frd, 'k', karray, oversupply_knnkpv_frd); title('Oversupply (FRU)');
-figure(); plot(karray, pshort_baseline_frd, 'k', karray, pshort_knnkpv_frd); title('Pshort (FRU)');
-figure(); plot(karray, oversupply_baseline_fru, 'k', karray, oversupply_knnkpv_fru); title('Oversupply (FRD)');
-figure(); plot(karray, pshort_baseline_fru, 'k', karray, pshort_knnkpv_fru); title('Pshort (FRD)');
+%% Produce figures
+figure(); 
+h = plot(karray, oversupply_knnkpv_frd./1E3, karray, oversupply_baseline_frd./1E3, 'k');
+set(h(1), 'Color', 'r');
+set(h(2), 'Color', 'g');
+set(h(3), 'Color', 'm');
+set(h(4), 'Color', 'b');
+set(h(5), 'Color', 'k');
+title('Oversupply (FRU)');
+xlabel('K');
+ylabel('Oversupply (GWh)');
+legend('\mu(k_{pv})', '\sigma(k_{pv})', 'v(k_{pv})', 'w(k_{pv})' ,'Baseline');
 
+figure(); 
+h = plot(karray, pshort_knnkpv_frd, karray, pshort_baseline_frd, 'k'); 
+set(h(1), 'Color', 'r');
+set(h(2), 'Color', 'g');
+set(h(3), 'Color', 'm');
+set(h(4), 'Color', 'b');
+set(h(5), 'Color', 'k');
+title('Pshort (FRU)');
+xlabel('K');
+ylabel('Probability of shortage');
+legend('\mu(k_{pv})', '\sigma(k_{pv})', 'v(k_{pv})', 'w(k_{pv})' ,'Baseline');
+
+figure(); 
+h = plot(karray, oversupply_knnkpv_fru./1E3, karray, oversupply_baseline_fru./1E3, 'k'); 
+set(h(1), 'Color', 'r');
+set(h(2), 'Color', 'g');
+set(h(3), 'Color', 'm');
+set(h(4), 'Color', 'b');
+set(h(5), 'Color', 'k');
+title('Oversupply (FRD)');
+xlabel('K');
+ylabel('Oversupply (GWh)');
+legend('\mu(k_{pv})', '\sigma(k_{pv})', 'v(k_{pv})', 'w(k_{pv})' ,'Baseline');
+
+figure(); 
+h = plot(karray, pshort_knnkpv_fru, karray, pshort_baseline_fru, 'k'); 
+set(h(1), 'Color', 'r');
+set(h(2), 'Color', 'g');
+set(h(3), 'Color', 'm');
+set(h(4), 'Color', 'b');
+set(h(5), 'Color', 'k');
+title('Pshort (FRD)');
+xlabel('K');
+ylabel('Probability of shortage');
+legend('\mu(k_{pv})', '\sigma(k_{pv})', 'v(k_{pv})', 'w(k_{pv})' ,'Baseline');
 
 % Pareto frontier
 figure(); 
@@ -177,9 +240,13 @@ set(h(2), 'MarkerFaceColor', 'g');
 set(h(3), 'MarkerFaceColor', 'm');
 set(h(4), 'MarkerFaceColor', 'b');
 set(h(5), 'MarkerFaceColor', 'k');
-title('FRU');
 xline(pshort_baseline_fru(karray==30), 'Color', 'r', 'LineStyle', '--');
 yline(oversupply_baseline_fru(karray==30)./1E3, 'Color', 'r', 'LineStyle', '--');
+title('FRU');
+xlabel('Probability of shortage');
+ylabel('Oversupply (GWh)');
+legend(h, '\mu(k_{pv})', '\sigma(k_{pv})', 'v(k_{pv})', 'w(k_{pv})' ,'Baseline');
+box on;
 
 figure(); 
 h(5)= scatter(pshort_baseline_frd, oversupply_baseline_frd./1E3, 60, 'ko');
@@ -192,7 +259,10 @@ set(h(2), 'MarkerFaceColor', 'g');
 set(h(3), 'MarkerFaceColor', 'm');
 set(h(4), 'MarkerFaceColor', 'b');
 set(h(5), 'MarkerFaceColor', 'k');
-title('FRD');
 xline(pshort_baseline_frd(karray==30), 'Color', 'r', 'LineStyle', '--');
 yline(oversupply_baseline_frd(karray==30)./1E3, 'Color', 'r', 'LineStyle', '--');
-
+title('FRD');
+xlabel('Probability of shortage');
+ylabel('Oversupply (GWh)');
+legend(h, '\mu(k_{pv})', '\sigma(k_{pv})', 'v(k_{pv})', 'w(k_{pv})' ,'Baseline');
+box on;
